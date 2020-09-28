538614cbcab0a8ed2ae102ffd2b003bf64860c32
Nelkor <nelkor9855118751@gmail.com>
2020-09-28 20:31:22 +0300

    small redesign

--- src/game/components/GameCanvas.vue
+++ src/game/components/GameCanvas.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="game-canvas"></div>
+  <canvas
+    class="overlay"
+    ref="canvas"
+  />
 </template>
 
 <script>
 export default {
   name: 'GameCanvas',
+  mounted() {
+    // const canvas = this.$refs.canvas
+    // const ctx = canvas.getContext('2d')
+
+    // Здесь можно инициализировать 2d-модуль
+  },
 }
 </script>
